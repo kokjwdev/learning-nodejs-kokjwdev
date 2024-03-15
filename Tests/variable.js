722be4ebf5a41784f9318cd89e3b51beff312d7c
KOK Dev <kok.jarukit@gmail.com>
2024-03-15 14:23:47 +0700

    commit

---
+++ Tests/variable.js
@@ -0,0 +1,19 @@
+var name0 = "Hello"
+console.log(name0);
+
+var name1 = "wayu"
+
+function group1() {
+    var name2 = "kok"
+}
+group1(); // การสั่งให้เรียกใช้งาน Functon Group1
+
+function group2() {
+    var name3 = "ame"
+}
+group2(); // การสั่งให้เรียกใช้งาน Functon Group2
+
+// เขียน console.log(); เพื่อแสดงค่าตอน Run Program ให้ค่าออกมาตามลำดับ
+// บรรทัดที่ 1. แสดง wayu
+// บรรทัดที่ 2. แสดง ame
+// บรรทัดที่ 3. แสดง kok
